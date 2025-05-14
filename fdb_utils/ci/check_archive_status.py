f68227c3f5d0fce2c7c878204dece20615938f10
Sam Weyermann <samuel.weyermann@meteoswiss.ch>
2025-05-14 10:03:40 +0200

    Create CI pipeline to validate the FDB archives (#7)
    Add and run a script that checks the archival status in FDB of the forecasts for an ICON model.
    
        Add the script and some rudimentary tests
        Add three Jenkins pipelines that run on CSCS Jenkins:
            A base pipeline that sets up the environment, runs the script, and sends a failure email
            A pipeline for each model (ICON-CH1-EPS and ICON-CH2-EPS) that runs the base script on a schedule

---
+++ fdb_utils/ci/check_archive_status.py
@@ -0,0 +1,314 @@
+import argparse
+import datetime as dt
+import logging
+import sys
+from dataclasses import dataclass
+from enum import IntEnum
+
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+from matplotlib.colors import ListedColormap
+from matplotlib.figure import Figure
+
+from fdb_utils.user.describe import list_all_values
+
+
+@dataclass
+class Collection:
+    model: str
+    members: int
+    steps: int
+    forecasts: int
+    interval: dt.timedelta
+    # How long after the start time of the run we expect the archive to be complete.
+    delay: dt.timedelta
+
+
+@dataclass
+class Parameter:
+    id: str
+    file_suffix: str
+    is_constant: bool
+    field_filter: dict[str, str]
+
+
+COLLECTIONS: dict[str, Collection] = {
+    "icon-ch1-eps": Collection(
+        model="icon-ch1-eps",
+        members=11,
+        steps=33,
+        forecasts=8,
+        interval=dt.timedelta(hours=3),
+        delay=dt.timedelta(hours=2, minutes=30),
+    ),
+    "icon-ch2-eps": Collection(
+        model="icon-ch2-eps",
+        members=21,
+        steps=121,
+        forecasts=4,
+        interval=dt.timedelta(hours=6),
+        delay=dt.timedelta(hours=3, minutes=30),
+    ),
+}
+
+
+# The poller archives the hourly rotlatlon grib files for constant params (suffix 'c'), single and multi level params
+# (no suffix), and params on pressure levels (suffix 'p'). Each file contains data for all associated parameters for a
+# single step and ensemble member. For further details on what each file type means, see:
+# https://meteoswiss.atlassian.net/wiki/spaces/APN/pages/412975206/ICON-22+PP+Naming+scheme+for+intermediate+products#TC-tasks%2C-prepare-step
+#
+# An error during archival will result in all data for that file missing. Thus we can check that all steps and members
+# are present for a single parameter that exists in the file to determine the status.
+PARAMS: list[Parameter] = [
+    Parameter(
+        id="500004", file_suffix="c", is_constant=True, field_filter={"levtype": "sfc"}
+    ),
+    Parameter(
+        id="500006",
+        file_suffix="p",
+        is_constant=False,
+        field_filter={"levelist": "200", "levtype": "pl"},
+    ),
+    Parameter(
+        id="500001",
+        file_suffix="",
+        is_constant=False,
+        field_filter={"levelist": "1", "levtype": "ml"},
+    ),
+]
+
+
+def last_run_time(collection: Collection, from_time: dt.datetime) -> dt.datetime:
+    # Adjust the current timestamp by the expected time for forecast run + archival so that a run of the script at any
+    # time is expected to succeed.
+    adjusted_timestamp = (from_time - collection.delay).timestamp()
+    run_interval = collection.interval.total_seconds()
+    last_run_ts = adjusted_timestamp // run_interval * run_interval
+    return dt.datetime.fromtimestamp(last_run_ts, tz=dt.timezone.utc)
+
+
+def get_param_status(
+    model: str, param: Parameter, date: str, time: str
+) -> list[list[int]]:
+    """Query FDB to determine the archival status for the parameter from the forecast at the provided time.
+
+    Returns a 2d array with dimensions [member, step] containing 1 if the data is present and a 0 if not.
+    """
+    num_members = COLLECTIONS[model].members
+    # Constant params are only defined on step 0, all others are defined for all steps.
+    if param.is_constant:
+        num_steps = 1
+    else:
+        num_steps = COLLECTIONS[model].steps
+    filter_values = {"param": param.id, "model": model, "date": date, "time": time}
+
+    status = []
+    for member in range(num_members):
+        param_filter = filter_values
+        param_filter["number"] = str(member)
+        param_filter |= param.field_filter
+        steps_present: set[str | int] = list_all_values(*["step"], **param_filter).get(
+            "step", set()
+        )
+        steps_status = [1 if str(s) in steps_present else 0 for s in range(num_steps)]
+        status.append(steps_status)
+
+    return status
+
+
+def get_archive_status(
+    model: str, forecast_time: dt.datetime
+) -> dict[str, list[list[int]]]:
+    """Check if each file of the forecast has been archived."""
+    date_str = forecast_time.strftime("%Y%m%d")
+    time_str = forecast_time.strftime("%H00")
+
+    archive_status = {}
+    for p in PARAMS:
+        param_status = get_param_status(model, p, date_str, time_str)
+        archive_status[p.file_suffix] = param_status
+    return archive_status
+
+
+def fx_filename(suffix: str, member: int, step: int) -> str:
+    """Construct the ICON fxshare filename for the provided parameters."""
+    filename_template = "_FXINP_lfrf{dd:02}{hh:02}000_{mmm:03}"
+    days = step // 24
+    hours = step % 24
+    filename = filename_template.format(dd=days, hh=hours, mmm=member) + suffix
+    return filename
+
+
+def get_failed_files(archive_status: dict[str, list[list[int]]]) -> list[str]:
+    failed_files = []
+    for file_suffix, param_status in archive_status.items():
+        for member, steps_status in enumerate(param_status):
+            for step, success in enumerate(steps_status):
+                if not success:
+                    failed_files.append(fx_filename(file_suffix, member, step))
+    return failed_files
+
+
+class ForecastStatus(IntEnum):
+    MISSING = 0
+    COMPLETE = 1
+    INCOMPLETE = 2
+
+
+def summary_status(archive_status: dict[str, list[list[int]]]) -> ForecastStatus:
+    """Determine the archival status of the forecast as a whole."""
+    any_success = False
+    all_success = True
+    for status in archive_status.values():
+        any_success |= any(any(steps_stat) for steps_stat in status)
+        all_success &= all(all(steps_stat) for steps_stat in status)
+
+    if all_success:
+        return ForecastStatus.COMPLETE
+    if any_success:
+        return ForecastStatus.INCOMPLETE
+    return ForecastStatus.MISSING
+
+
+def historical_summary_status(
+    last_run_start: dt.datetime, collection: Collection
+) -> tuple[list[ForecastStatus], list[str]]:
+    """Return the summary status for all past forecasts that should still exist."""
+    history_status = []
+    history_datetime = []
+    past_start = last_run_start
+    for _ in range(1, collection.forecasts):
+        past_start = past_start - collection.interval
+        past_status = get_archive_status(collection.model, past_start)
+        history_status.append(summary_status(past_status))
+        history_datetime.append(past_start.strftime("%y%m%d%H00"))
+    return history_status, history_datetime
+
+
+def plot_status(ax: Axes, status: list[list[int]], file_suffix: str) -> None:
+    cmap = ListedColormap(["red", "green"])
+    num_members = len(status)
+    num_steps = len(status[0])
+    ax.set_anchor("W")
+    ax.set_aspect("equal")
+    ax.set_title(f"Files _FXINP_lfrf<DDHH>0000_<mmm>{file_suffix}", loc="left")
+    ax.set_xlabel("step")
+    ax.set_xticks(
+        [x + 0.5 for x in range(num_steps)], labels=[str(s) for s in range(num_steps)]
+    )
+    ax.set_ylabel("member")
+    ax.set_yticks(
+        [x + 0.5 for x in range(num_members)],
+        labels=[str(m) for m in range(num_members)],
+    )
+    ax.pcolormesh(
+        status, cmap=cmap, shading="flat", edgecolors="k", linewidths=1, vmin=0, vmax=1
+    )
+
+
+def plot_history(
+    ax: Axes, history_status: list[ForecastStatus], history_datetime: list[str]
+) -> None:
+    # Plot the historical archival status.
+    cmap = ListedColormap(["red", "green", "orange"])
+    ax.set_anchor("W")
+    ax.set_aspect("equal")
+    ax.set_title("Historical archive status", loc="left")
+    ax.set_xlabel("Archive date/time")
+    ax.set_xticks(
+        [x + 0.5 for x in range(len(history_datetime))], labels=history_datetime
+    )
+    ax.set_yticks([], [])
+    ax.pcolormesh(
+        [history_status],
+        cmap=cmap,
+        shading="flat",
+        edgecolors="k",
+        linewidths=1,
+        vmin=0,
+        vmax=2,
+    )
+
+
+def create_figure(collection: Collection) -> tuple[Figure, list[Axes]]:
+    # Size the figure so the subplots have square boxes of the same size.
+    boxes_per_inch = 2.5
+    subplot_height = collection.members / boxes_per_inch
+    # Use a larger box for the historical status to prevent the longer labels from overlapping.
+    historical_box_size = 1.5
+    # Add height for the historical plot and vertical spacing between subplots.
+    plot_height = len(PARAMS) * subplot_height + historical_box_size + len(PARAMS)
+    height_ratios = [subplot_height for _ in PARAMS]
+    height_ratios.append(historical_box_size)
+    plot_width = collection.steps / boxes_per_inch
+
+    return plt.subplots(
+        len(PARAMS) + 1,
+        figsize=(plot_width, plot_height),
+        gridspec_kw={"height_ratios": height_ratios},
+        layout="constrained",
+    )
+
+
+def main(model: str) -> bool:
+    collection = COLLECTIONS[model]
+    last_run_start = last_run_time(collection, dt.datetime.now(dt.timezone.utc))
+    latest_archive_status = get_archive_status(model, last_run_start)
+
+    # For past forecasts, we have the full details already in previous runs. We only want to detect and alert if a
+    # forecast is deleted early.
+    history_status, history_datetime = historical_summary_status(
+        last_run_start, collection
+    )
+    history_status.insert(0, summary_status(latest_archive_status))
+    history_datetime.insert(0, last_run_start.strftime("%y%m%d%H00"))
+
+    # Plot the archival status.
+    fig, axs = create_figure(collection)
+    fig.suptitle(
+        f"Archival status for {model} run {last_run_start.strftime('%y%m%d%H00')}"
+    )
+
+    # Plot a status grid for each file suffix.
+    for ax, param in zip(axs, PARAMS):
+        plot_status(ax, latest_archive_status[param.file_suffix], param.file_suffix)
+
+    plot_history(axs[len(PARAMS)], history_status, history_datetime)
+
+    plt.savefig(
+        f"heatmap_{model}_{last_run_start.strftime('%y%m%d%H00')}.png",
+        bbox_inches="tight",
+    )
+
+    # If any files in the latest forecast failed, print the names and return failure.
+    if history_status[0] != ForecastStatus.COMPLETE:
+        logging.warning(
+            "The following files failed to archive: %s",
+            get_failed_files(latest_archive_status),
+        )
+        return False
+
+    if any(status == ForecastStatus.MISSING for status in history_status):
+        # Only report failure on missing forecast since an incomplete forecast will have
+        # alerted us already.
+        logging.warning(
+            "The forecast is missing for the following dates: %s",
+            [
+                date_str
+                for date_str, status in zip(history_datetime, history_status)
+                if status == ForecastStatus.MISSING
+            ],
+        )
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "model", type=str.lower, choices=["icon-ch1-eps", "icon-ch2-eps"]
+    )
+    args = parser.parse_args()
+
+    if not main(args.model):
+        sys.exit(1)
---
+++ test/test_check_archive_status.py
@@ -0,0 +1,234 @@
+import datetime as dt
+
+import matplotlib.pyplot as plt
+import pytest
+from unittest.mock import patch
+
+import fdb_utils.ci.check_archive_status as cas
+
+
+def test_overall_status_missing():
+    status_dict = {
+        "p1": [[0, 0, 0, 0], [0, 0, 0, 0]],
+        "p2": [[0], [0], [0], [0]],
+    }
+    assert cas.summary_status(status_dict) == cas.ForecastStatus.MISSING
+
+
+def test_overall_status_complete():
+    status_dict = {
+        "p1": [[1, 1, 1, 1], [1, 1, 1, 1]],
+        "p2": [[1], [1], [1], [1]],
+    }
+    assert cas.summary_status(status_dict) == cas.ForecastStatus.COMPLETE
+
+
+def test_overall_status_incomplete():
+    status_dict = {
+        "p1": [[1, 1, 1, 0], [1, 1, 1, 1]],
+        "p2": [[1], [1], [1], [1]],
+    }
+    assert cas.summary_status(status_dict) == cas.ForecastStatus.INCOMPLETE
+
+
+def test_fx_filename():
+    assert cas.fx_filename("suf", 0, 0) == "_FXINP_lfrf0000000_000suf"
+    assert cas.fx_filename("", 1, 10) == "_FXINP_lfrf0010000_001"
+    assert cas.fx_filename("s", 123, 49) == "_FXINP_lfrf0201000_123s"
+
+
+def test_get_failed_files():
+    success_dict = {
+        "suf1": [[1, 1, 0], [0, 1, 1]],
+        "suf2": [[0, 1, 1], [1, 1, 1], [1, 0, 1]],
+    }
+    expected_files = [
+        "_FXINP_lfrf0002000_000suf1",
+        "_FXINP_lfrf0000000_001suf1",
+        "_FXINP_lfrf0000000_000suf2",
+        "_FXINP_lfrf0001000_002suf2",
+    ]
+    assert cas.get_failed_files(success_dict) == expected_files
+
+
+def test_last_run_time_ch1():
+    icon_1 = cas.COLLECTIONS["icon-ch1-eps"]
+
+    # Verify that ICON-CH1 run times are every three hours.
+    first_time_ch1 = dt.datetime.fromisoformat("2025-01-01T02:45Z")
+    first_run_time = dt.datetime.fromisoformat("2025-01-01T00:00Z")
+    assert cas.last_run_time(icon_1, first_time_ch1) == first_run_time
+    assert (
+        cas.last_run_time(icon_1, first_time_ch1 + dt.timedelta(hours=1))
+        == first_run_time
+    )
+    assert (
+        cas.last_run_time(icon_1, first_time_ch1 + dt.timedelta(hours=2))
+        == first_run_time
+    )
+    assert cas.last_run_time(
+        icon_1, first_time_ch1 + dt.timedelta(hours=3)
+    ) == first_run_time + dt.timedelta(hours=3)
+    assert cas.last_run_time(
+        icon_1, first_time_ch1 + dt.timedelta(hours=6)
+    ) == first_run_time + dt.timedelta(hours=6)
+    assert cas.last_run_time(
+        icon_1, first_time_ch1 + dt.timedelta(hours=7)
+    ) == first_run_time + dt.timedelta(hours=6)
+
+
+def test_last_run_time_ch2():
+    icon_2 = cas.COLLECTIONS["icon-ch2-eps"]
+
+    # Verify that ICON-CH2 run times are every six hours.
+    first_time_ch2 = dt.datetime.fromisoformat("2025-01-01T03:45Z")
+    first_run_time = dt.datetime.fromisoformat("2025-01-01T00:00Z")
+    assert cas.last_run_time(icon_2, first_time_ch2) == first_run_time
+    assert (
+        cas.last_run_time(icon_2, first_time_ch2 + dt.timedelta(hours=1))
+        == first_run_time
+    )
+    assert (
+        cas.last_run_time(icon_2, first_time_ch2 + dt.timedelta(hours=3))
+        == first_run_time
+    )
+    assert (
+        cas.last_run_time(icon_2, first_time_ch2 + dt.timedelta(hours=5))
+        == first_run_time
+    )
+    assert cas.last_run_time(
+        icon_2, first_time_ch2 + dt.timedelta(hours=6)
+    ) == first_run_time + dt.timedelta(hours=6)
+    assert cas.last_run_time(
+        icon_2, first_time_ch2 + dt.timedelta(hours=7)
+    ) == first_run_time + dt.timedelta(hours=6)
+
+
+def test_last_run_time_lead_time():
+    icon_1 = cas.COLLECTIONS["icon-ch1-eps"]
+
+    # Verify that the lead time is taken into account.
+    first_time_lead_time = dt.datetime.fromisoformat("2025-01-01T03:30Z")
+    first_run_time = dt.datetime.fromisoformat("2025-01-01T00:00Z")
+    assert cas.last_run_time(icon_1, first_time_lead_time) == first_run_time
+
+
+def test_create_figure():
+    fig, axs = cas.create_figure(cas.COLLECTIONS["icon-ch1-eps"])
+    assert fig is not None
+    assert len(axs) == 4
+
+
+def test_plot_status():
+    status = [[1, 1, 1], [1, 1, 1], [1, 1, 0], [1, 0, 1]]
+    _, ax = plt.subplots()
+    cas.plot_status(ax, status, "suf")
+    assert "suf" in ax.get_title("left")
+    assert ax.get_xlabel() == "step"
+    assert [x.get_text() for x in ax.get_xticklabels()] == ["0", "1", "2"]
+    assert ax.get_ylabel() == "member"
+    assert [y.get_text() for y in ax.get_yticklabels()] == ["0", "1", "2", "3"]
+
+
+def test_plot_history():
+    status = [
+        cas.ForecastStatus.COMPLETE,
+        cas.ForecastStatus.INCOMPLETE,
+        cas.ForecastStatus.MISSING,
+    ]
+    labels = ["2501010900", "2501010600", "2501010300"]
+    _, ax = plt.subplots()
+    cas.plot_history(ax, status, labels)
+    assert "Historical" in ax.get_title("left")
+    assert "date" in ax.get_xlabel()
+    assert [x.get_text() for x in ax.get_xticklabels()] == labels
+    assert len(ax.get_yticks()) == 0
+
+
+def return_steps(missing_values: dict[tuple[str], dict[str, list[int]]]):
+    def list_all_values_mock(*filter_keys: str, **filter_by_values: str):
+        model = filter_by_values["model"]
+        param = filter_by_values["param"]
+        num_steps = 1 if param == "500004" else cas.COLLECTIONS[model].steps
+
+        number = filter_by_values["number"]
+        date = filter_by_values["date"]
+        time = filter_by_values["time"]
+        missing_steps = missing_values.get((param, date, time), {}).get(number, [])
+        steps = []
+        for s in range(num_steps):
+            if s not in missing_steps:
+                steps.append(str(s))
+        return {"step": steps}
+
+    return list_all_values_mock
+
+
+@patch("fdb_utils.ci.check_archive_status.list_all_values")
+def test_get_archive_status(list_values, tmp_path, data_dir):
+    missing_values = {
+        ("500004", "20250202", "0300"): {"0": [0], "1": [0]},
+        ("500006", "20250202", "0300"): {"0": [30, 31], "9": [0, 1]},
+        ("500001", "20250202", "0300"): {"1": [0], "2": [10]},
+    }
+    list_values.side_effect = return_steps(missing_values)
+
+    forecast_time = dt.datetime.fromisoformat("2025-02-02T03:00Z")
+    archive_status = cas.get_archive_status("icon-ch1-eps", forecast_time)
+
+    assert "c" in archive_status.keys()
+    assert "p" in archive_status.keys()
+    assert "" in archive_status.keys()
+    assert archive_status["c"][0][0] == 0
+    assert archive_status["c"][1][0] == 0
+    assert archive_status["p"][0][30] == 0
+    assert archive_status["p"][0][31] == 0
+    assert archive_status["p"][9][0] == 0
+    assert archive_status["p"][9][1] == 0
+    assert archive_status[""][1][0] == 0
+    assert archive_status[""][2][10] == 0
+    # No other files are missing.
+    status_sum = 0
+    for param_status in archive_status.values():
+        status_sum += sum(sum(row) for row in param_status)
+    assert status_sum == 11 + (11 * 33) + (11 * 33) - 8
+
+
+@patch("fdb_utils.ci.check_archive_status.list_all_values")
+def test_historical_archive_status(list_values, tmp_path, data_dir):
+    # Set one incomplete forecast and one missing forecast.
+    missing_values = {
+        ("500004", "20250202", "0000"): {"0": [0]},
+        ("500004", "20250201", "2100"): {str(n): [0] for n in range(11)},
+        ("500006", "20250201", "2100"): {
+            str(n): [s for s in range(33)] for n in range(11)
+        },
+        ("500001", "20250201", "2100"): {
+            str(n): [s for s in range(33)] for n in range(11)
+        },
+    }
+    list_values.side_effect = return_steps(missing_values)
+
+    first_forecast_time = dt.datetime.fromisoformat("2025-02-02T03:00Z")
+    historical_summary, historical_dates = cas.historical_summary_status(
+        first_forecast_time, cas.COLLECTIONS["icon-ch1-eps"]
+    )
+
+    assert historical_summary == [
+        cas.ForecastStatus.INCOMPLETE,
+        cas.ForecastStatus.MISSING,
+        cas.ForecastStatus.COMPLETE,
+        cas.ForecastStatus.COMPLETE,
+        cas.ForecastStatus.COMPLETE,
+        cas.ForecastStatus.COMPLETE,
+        cas.ForecastStatus.COMPLETE,
+    ]
+    assert historical_dates == [
+        "2502020000",
+        "2502012100",
+        "2502011800",
+        "2502011500",
+        "2502011200",
+        "2502010900",
+        "2502010600",
+    ]
