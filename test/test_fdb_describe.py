308898e7ff5ebd8e4192b61015a402bea14594f1
nnellen <193914323+nnellen@users.noreply.github.com>
2025-11-14 18:25:58 +0100

    remove code duplicates

--- test/test_fdb_describe.py
+++ test/test_fdb_describe.py
@@ -6,20 +6,23 @@ from fdb_utils.user.describe import list_all_values, get_archived_forecasts,SCHE
 from test.utils import _generate_file_to_upload, _modify_grib_file
 from test.conftest import fdb
 
+
 def test_list_all_values(tmp_path, data_dir, fdb):
 
     # Generate GRIB files with different dates and archive to FDB
-    file_to_upload_1, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
-    file_to_upload_2, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
-    file_to_upload_3, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
-    file_to_upload_4, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
 
-    _modify_grib_file(file_to_upload_1, date='20240202', time='300', number=5, step=0)
-    _modify_grib_file(file_to_upload_2, date='20240202', time='300', number=5, step=1)
-    _modify_grib_file(file_to_upload_3, date='20240203', time='300', number=1, step=2)
-    _modify_grib_file(file_to_upload_4, date='20240203', time='600', number=2, step=3)
+    files = []
 
-    for file in (file_to_upload_1, file_to_upload_2, file_to_upload_3, file_to_upload_4):
+    for i in range(4):
+        file, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
+        files.append(file)
+
+    _modify_grib_file(files[0], date='20240202', time='300', number=5, step=0)
+    _modify_grib_file(files[1], date='20240202', time='300', number=5, step=1)
+    _modify_grib_file(files[2], date='20240203', time='300', number=1, step=2)
+    _modify_grib_file(files[3], date='20240203', time='600', number=2, step=3)
+
+    for file in files:
         with open(file, "rb") as f:
             fdb.archive(f.read())
 
@@ -35,17 +38,18 @@ def test_list_all_values(tmp_path, data_dir, fdb):
 def test_listall_values_filtered(tmp_path, data_dir, fdb):
 
     # Generate GRIB files with different dates and archive to FDB
-    file_to_upload_1, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
-    file_to_upload_2, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
-    file_to_upload_3, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
-    file_to_upload_4, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
+    files = []
 
-    _modify_grib_file(file_to_upload_1, date='20240202', time='300', number=5, step=0)
-    _modify_grib_file(file_to_upload_2, date='20240202', time='300', number=5, step=1)
-    _modify_grib_file(file_to_upload_3, date='20240203', time='300', number=1, step=2)
-    _modify_grib_file(file_to_upload_4, date='20240203', time='600', number=2, step=3)
+    for i in range(4):
+        file, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
+        files.append(file)
 
-    for file in (file_to_upload_1, file_to_upload_2, file_to_upload_3, file_to_upload_4):
+    _modify_grib_file(files[0], date='20240202', time='300', number=5, step=0)
+    _modify_grib_file(files[1], date='20240202', time='300', number=5, step=1)
+    _modify_grib_file(files[2], date='20240203', time='300', number=1, step=2)
+    _modify_grib_file(files[3], date='20240203', time='600', number=2, step=3)
+
+    for file in files:
         with open(file, "rb") as f:
             fdb.archive(f.read())
 
@@ -61,18 +65,19 @@ def test_get_archived_forecasts(data_dir, tmp_path, fdb):
 
 
     #generate some files with different dates and archive to FDB
-    file_to_upload_1, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
-    file_to_upload_2, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
-    file_to_upload_3, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
+    files = []
 
-    _modify_grib_file(file_to_upload_1, date='20240202', time='300')
-    _modify_grib_file(file_to_upload_2, date='20240202', time='600')
-    _modify_grib_file(file_to_upload_3, date='20240302', time='900')
+    for i in range(3):
+        file, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True)
+        files.append(file)
 
-    with open(file_to_upload_1, "rb") as f:
-        fdb.archive(f.read())
-    with open(file_to_upload_2, "rb") as f:
-        fdb.archive(f.read())
+    _modify_grib_file(files[0], date='20240202', time='300')
+    _modify_grib_file(files[1], date='20240202', time='600')
+    _modify_grib_file(files[2], date='20240302', time='900')
+
+    for file in files[:2]:
+        with open(file, "rb") as f:
+            fdb.archive(f.read())
 
     fdb.flush()
 
@@ -80,7 +85,7 @@ def test_get_archived_forecasts(data_dir, tmp_path, fdb):
 
     assert result == [datetime(2024, 2, 2, 3), datetime(2024, 2, 2, 6)]
 
-    with open(file_to_upload_3, "rb") as f:
+    with open(files[2], "rb") as f:
         fdb.archive(f.read())
     fdb.flush()
 
@@ -124,15 +129,17 @@ def test_get_archived_forecast_empty_request(tmp_path, data_dir, fdb):
 
 def test_list_all_values_levelist_sorting(tmp_path, data_dir, fdb):
 
-    file_to_upload_1, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True, levelist=True)
-    file_to_upload_2, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True, levelist=True)
-    file_to_upload_3, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True, levelist=True)
+    files = []
 
-    _modify_grib_file(file_to_upload_1, date='20240202', time='300', level=float(1), step=3)
-    _modify_grib_file(file_to_upload_2, date='20240203', time='600', level=float(3), step=3)
-    _modify_grib_file(file_to_upload_3, date='20240203', time='900', level=float(2), step=3)
+    for i in range(3):
+        file, _, _ = _generate_file_to_upload(tmp_path, data_dir, random=True, levelist=True)
+        files.append(file)
 
-    for file in (file_to_upload_1, file_to_upload_2, file_to_upload_3):
+    _modify_grib_file(files[0], date='20240202', time='300', level=float(1), step=3)
+    _modify_grib_file(files[1], date='20240203', time='600', level=float(3), step=3)
+    _modify_grib_file(files[2], date='20240203', time='900', level=float(2), step=3)
+
+    for file in files:
         with open(file, "rb") as f:
             fdb.archive(f.read())
 
